f74b9b563d867ae9711022abc2cb02792cc5ce9d
RatchetHamster <94993719+RatchetHamster@users.noreply.github.com>
2024-09-26 14:20:25 +0100

    Update install.sh

--- mp3/install.sh
+++ mp3/install.sh
@@ -3,16 +3,16 @@
 # To install run: 
 # [You may need to install pip install git if lite version of rasbian]
 # git clone https://github.com/RatchetHamster/pirate-mp3-enhanced_options.git
-# /home/pi/pirate-mp3/install.sh
+# /home/pi/pirate-mp3-enhanced_options/install.sh
 
 # Update and upgrade
 sudo apt update
 sudo apt upgrade
 
 # Create, activate and install packages on virtural python env. 
-python3 -m .venv /home/pi/pirate-mp3
-source /home/pi/pirate-mp3/.venv/bin/activate
-pip3 install /home/pi/pirate-mp3/mp3/requirements.txt
+python3 -m .venv /home/pi/pirate-mp3-enhanced_options
+source /home/pi/pirate-mp3-enhanced_options/.venv/bin/activate
+pip3 install /home/pi/pirate-mp3-enhanced_options/mp3/requirements.txt
 
 # Modify /boot/fireware/config.txt (get sound working on Pirate-Audio)
 function add_to_config_text {
@@ -27,7 +27,7 @@ add_to_config_text "dtoverlay=hifiberry-dac" /boot/firmware/config.txt 5
 
 # Run on boot (modify /etc/rc.local so that module runs on boot)
 # check: /tmp/rc.local.log for errors, warnings and outputs
-add_to_config_text 'su --command "PYTHONPATH=\/home\/pi\/pirate-mp3 \/home\/pi\/pirate-mp3\/.venv\/bin\/python3 -m mp3" --login pi' /etc/rc.local 19
+add_to_config_text 'su --command "PYTHONPATH=\/home\/pi\/pirate-mp3-enhanced_options \/home\/pi\/pirate-mp3-enhanced_options\/.venv\/bin\/python3 -m mp3" --login pi' /etc/rc.local 19
 add_to_config_text "set -x" /etc/rc.local 19
 add_to_config_text "exec 1>&2" /etc/rc.local 19
 add_to_config_text "exec 2> \/tmp\/rc.local.log" /etc/rc.local 19
