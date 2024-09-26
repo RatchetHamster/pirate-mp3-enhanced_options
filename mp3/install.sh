37cb81ceec2b38a35d75f57937a23e73d28d02d3
RatchetHamster <94993719+RatchetHamster@users.noreply.github.com>
2024-09-26 19:06:18 +0100

    Update install.sh

--- mp3/install.sh
+++ mp3/install.sh
@@ -8,6 +8,8 @@
 # Update and upgrade
 sudo apt update
 sudo apt upgrade
+sudo apt install git
+sudo apt-get install python3-pip
 sudo apt-get pulseaudio
 
 # Create, activate and install packages on virtural python env. 
