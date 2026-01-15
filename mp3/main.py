473a8fe328efb3d2fbec024ec90951b2d63dd9e8
RatchetHamster <94993719+RatchetHamster@users.noreply.github.com>
2026-01-15 19:07:39 +0000

    Update main.py

--- mp3/main.py
+++ mp3/main.py
@@ -5,9 +5,7 @@ from core import Core
 #region Main
 
 def main():
-    print(1)
     Core().setup()
-    print(2)
     AUDIO = "/home/pi/pirate-mp3-enhanced_options/music"
     library = Library(AUDIO)
     frontend = Frontend(library)
@@ -16,5 +14,7 @@ def main():
         library.auto_next(auto_track_next=True, auto_album_next=True)
         frontend.update_frame()
         frontend.check_sleep_idle()
-        
+
+main()
+
 #endregion
