4b57d03f0cfd8ef9de793a6213a62f505cda8584
RatchetHamster <94993719+RatchetHamster@users.noreply.github.com>
2026-01-15 19:07:19 +0000

    Update main.py

--- mp3/main.py
+++ mp3/main.py
@@ -5,7 +5,9 @@ from core import Core
 #region Main
 
 def main():
+    print(1)
     Core().setup()
+    print(2)
     AUDIO = "/home/pi/pirate-mp3-enhanced_options/music"
     library = Library(AUDIO)
     frontend = Frontend(library)
