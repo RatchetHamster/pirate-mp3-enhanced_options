770243b4f66a883e4d6c2f73db005bf792844575
RatchetHamster <94993719+RatchetHamster@users.noreply.github.com>
2026-01-15 18:39:54 +0000

    Update hardware.py

--- mp3/hardware.py
+++ mp3/hardware.py
@@ -1,5 +1,5 @@
 import time
-from gpiozero import PMWLED, Button
+from gpiozero import PWMLED, Button
 from ST7789 import ST7789
 import logging
 #Logger:
@@ -120,5 +120,6 @@ class Board(Screen, Buttons):
         self.is_shutdown = False
 
 #endregion
+
 
 
