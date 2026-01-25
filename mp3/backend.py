5900a1936c73aeb333382a3913ffead3d0e32084
RatchetHamster <94993719+RatchetHamster@users.noreply.github.com>
2026-01-25 19:37:54 +0000

    Refactor Track class to initialize ID3 tags correctly

--- mp3/backend.py
+++ mp3/backend.py
@@ -27,9 +27,8 @@ class Track:
 
     def check_and_fix_tag(self):
         if self.id3 == None:
-            self.id3 = eyed3.load(self.path, tag_version(2,3,0))
-            self.id3.tag = eyed3.id3.Tag()
-            self.id3.file_info = eyed3.id3.FileInfo(f"{self.path.stem}")
+            self.id3 = eyed3.load(self.path)
+            self.id3.initTag()
             self.id3.tag.artist = "A"
             self.id3.tag.title = f"{self.path.stem[:-4]}"
             self.id3.save()
@@ -167,6 +166,7 @@ class Library:
                 else:
                     self.albums[self.current_index].next()
                 self.play()
+
 
 
 
